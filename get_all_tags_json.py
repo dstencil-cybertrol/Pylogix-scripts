a7fbdac8694c0882bb2108476b546f0947775259
dstencil-cybertrol <104449382+dstencil-cybertrol@users.noreply.github.com>
2022-06-29 19:38:55 -0500

    formatted script to more readable and usable format

--- get_all_tags_json.py
+++ get_all_tags_json.py
@@ -1,32 +1,40 @@
 #!/usr/bin/env python3
 
 import datetime
-from pylogix import PLC
 import json
-
+rom pylogix import PLC
 
 IP = input('Enter AB PLC IP Address xxx.xxx.xxx.xxx \n')
 microstatus = input('is PLC a Micro800 series? (y or n) def(n) \n')
-if microstatus == 'y' or 'yes':
+if microstatus == 'y':
     microstatus = True
 else:
     microstatus = False
 dateformat = datetime.datetime.now()
-files = "{}_{}.json".format(IP,dateformat.strftime("%m%d%y"))
-with PLC(IP) as comm:
-        
+date_formatted = dateformat.strftime('%m%d%y')
+files = f"{IP}_{date_formatted}".json
+with PLC() as comm:
+        comm.IPAddress = IP
+        tags = comm.GetTagList()
+        output_tags = {IP: []}
+
         comm.Micro800 = microstatus
-       
-        with open(files, 'w') as txt: 
-            tags = comm.GetTagList()
-            tagdict = {IP: []}
+        comm.GetTagList()
+
+        for t in tags.Value:
+            t_read = comm.Read(t.TagName)
+            t_value = t_read.Value
+            #if type(t_value) == bytes:
+                #tag_value = f'{tag_value}'
+
+            tag_dict = {
+                "Value":     t_value, 
+                "Data Type": t.DataType,
+                "Status":    t.Status
+            }
+            output_tags[IP].append({t_read.TagName: tag_dict})
             
-            for t in tags.Value:
-                tagread = comm.Read(t.TagName)
-                tagdict[IP].append({tagread.TagName: {'Value': tagread.Value,'Data Type':t.DataType,'Status':tagread.Status}})
-                
-                print('Writing: {} \n'.format(tagdict))
-            json.dump(tagdict,txt, indent = 4)
-            
-  
-        print('Tag Data written to {}'.format(files))
+            print(f'Writing: {t_read.TagName}: {tag_dict}')
+
+json.dump(output_tags, open(files, "w"), indent=4)
+print(f'Tag Data written to {files}')
